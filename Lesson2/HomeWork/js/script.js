0aeb89fd5b562d1bee868462a574945609eb8405
ppvictoriaaa <pekurvika@gmail.com>
2024-07-01 13:23:32 +0300

    Les2

---
+++ Lesson2/HomeWork/js/script.js
@@ -0,0 +1,60 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// let num = -3;
+// if (num >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// let constant = "test";
+// if (constant === "test") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// let num = Number(prompt("enter number"));
+// if (num > 10) {
+//   console.log(num - 5);
+// } else if (num < 10) {
+//   console.log(num + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// let numMonth = Number(prompt("enter number and i say the month"));
+// switch (numMonth) {
+//   case 1:
+//     alert(`the number ${numMonth} is January`);
+//     break;
+//   case 2:
+//     alert(`the number ${numMonth} is February`);
+//     break;
+//   case 3:
+//     alert(`the number ${numMonth} is March`);
+//     break;
+//   case 4:
+//     alert(`the number ${numMonth} is April`);
+//     break;
+//   case 5:
+//     alert(`the number ${numMonth} is May`);
+//     break;
+//   default:
+//     alert("we have only 5 months");
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// numStr = prompt("enter three-digit number");
+// alert(Number(numStr[0]) + Number(numStr[1]) + Number(numStr[2]));
